d6bb6805d29cc89026a3b808f2fbd0e4498f5759
Jason Bernert <jasonbernert@gmail.com>
2020-01-05 21:32:23 -0500

    lots of random explorations

--- js/blob.js
+++ js/blob.js
@@ -3,7 +3,7 @@ AFRAME.registerComponent('blob', {
     width: {type: 'number', default: 1},
     height: {type: 'number', default: 128},
     depth: {type: 'number', default: 128},
-    color: {type: 'color', default: '#AAA'},
+    color: {type: 'color', default: '#FFF'},
     speed: {type: 'number', defualt: 0.001},
     spikes: {type: 'number', defualt: 2},
   },
@@ -14,12 +14,12 @@ AFRAME.registerComponent('blob', {
     const data = this.data;
     const el = this.el;
     this.geometry = new THREE.SphereGeometry(data.width, data.height, data.depth);
-    this.material = new THREE.MeshNormalMaterial({color: data.color});
+    this.material = new THREE.MeshStandardMaterial({color: data.color, roughness: 1, metalness: 0});
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     el.setObject3D('mesh', this.mesh);
   },
 
-  tick: function (time, timeDelta) {
+  tick: function (time) {
     const spikes = this.data.spikes;
     const speed = this.data.speed;
     for (let i = 0; i < this.geometry.vertices.length; i++) {
